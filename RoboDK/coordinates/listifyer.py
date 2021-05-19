78899e8ce3ef42dfeba3d45c8373c023002bd94f
DGames95 <d.l.games@student.tudelft.nl>
2021-05-19 17:31:35 +0200

    Update listifyer.py

--- RoboDK/coordinates/listifyer.py
+++ RoboDK/coordinates/listifyer.py
@@ -2,6 +2,11 @@ data = open('wingbox_robodk_coordinates generator txt formatted.txt')
 table = []
 
 
+# Coordinates of the corner of table
+x = 1000
+y = 1000
+z = 1000
+
 # read table and get rid of fat
 for line in data:
     row = line.split('\t')
@@ -14,6 +19,9 @@ for line in data:
             rowclean.append(item)      
     del rowclean[3]
     res = [float(i) for i in rowclean]
+    res[0] += x
+    res[1] += y
+    res[2] += z
     table.append(res)
 
     
